9e83c84f037424ca8f6585ff7de0bbd1790b1273
berniegamarra <berniegamarra@gmail.com>
2022-05-31 13:07:40 -0500

    resuelto pero con 3 ejercicios que no se resuelven aun con la solución de la pagina

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -175,13 +175,13 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (num === 0) {
-    return "false";
-  } else if (num > 0) {
+  if (num > 0) {
     return "Es positivo";
+  } else if (num < 0) {
+    return "Es negativo";
   } 
   else {
-    return "Es negativo"
+    return "false";
   }
 }
 
